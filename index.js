b17662f899e34f233e2a0d46797b38348afeae8a
TanzilaMomin <147149600+TanzilaMomin@users.noreply.github.com>
2023-11-01 19:58:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document .write("<h1> Java script Name </h1>");
+var First=prompt("Enter First Name :");
+var Middle=prompt("Enter Middle Name :");
+var Last=prompt("Enter Last Name :");
+document.write("<span>Full Name:</span>");
+document.write("<b>"+First+" </b>");
+document.write("<b>"+Middle+" </b>");
+document.write("<b>"+Last+"</b>");
